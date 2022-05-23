96e6d22349042770f56981e7cbc915d843ab3531
chanwoosung <chanwoosung@naver.com>
2022-05-23 09:12:10 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Navbar from "../components/navbar.js"
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (<div>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </div>
+    )
 }
 
 export default MyApp
---
+++ pages/help.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+import Title from "../components/Title";
+
+export default function test(){
+    return (
+        <div>
+            <Title title="help" />
+            <span>HELP ME</span>
+
+        </div>
+    );
+}
